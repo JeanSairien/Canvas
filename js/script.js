2d6bf169c6b06cb1a872bc1f5767d55566397c9c
dirago <diragor@gmail.com>
2016-08-23 02:20:47 +0200

    create header + nav + logo

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var header = document.querySelector('header');
+var headerContent = document.getElementById('header');
+var bigLogo = document.getElementById('logo_normal');
+var stretchLogo = document.getElementById('logo_stretch');
+var main = document.querySelector('main');
+
+var scrollY = function() {
+    var supportPageOffset = window.pageXOffset !== undefined;
+    var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+    return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+};
+var onScroll = function() {
+    if (scrollY() > 70) {
+        header.style.position = "fixed";
+        header.style.height = "50px";
+        headerContent.style.height = "50px";
+        bigLogo.style.display = "none";
+        stretchLogo.style.display = "block";
+        main.style.paddingTop = "130px";
+
+    } else {
+        header.style.position = "relative";
+        header.style.height = "110px";
+        headerContent.style.height = "110px";
+        bigLogo.style.display = "block";
+        stretchLogo.style.display = "none";
+        main.style.paddingTop = "0px";
+    }
+};
+window.addEventListener('scroll', onScroll);
