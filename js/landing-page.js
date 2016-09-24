928181361bc837358d26b3adf801a584d0e0298e
dirago <diragor@gmail.com>
2016-09-24 19:33:16 +0200

    footer created

--- js/landing-page.js
+++ js/landing-page.js
@@ -2,14 +2,17 @@ $(document).ready(function(){
     $landing = $('.landing');
     $header = $('header');
     $main = $('main');
+    $footer = $('footer');
     if (document.cookie === "") {
         document.cookie = "No need to relaunch the canvas landing page bro' :)"
         $header.hide();
         $main.hide();
+        $footer.hide();
         function launchWebsite(){
-            $landing.fadeOut('slow');
-            $header.fadeIn('slow')
-            $main.fadeIn('slow')
+            $landing.fadeOut(1000);
+            $header.fadeIn(3000);
+            $main.fadeIn(3000);
+            $footer.fadeIn(3000);
         }
         setTimeout(launchWebsite, 9000);
 
---
+++ js/signature.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('object').mouseenter(function(){
+        $(this).after('<p id="bubble1" class="bubble bubble1"></p>');
+        $(this).after('<p id="bubble2" class="bubble bubble2"></p>');
+        $(this).after('<p id="bubble3" class="bubble bubble-text animated pulse">Guess who\'s back !</p>');
+        $('#bubble1').fadeIn('slow');
+        $('#bubble2').delay(500).fadeIn('slow');
+        $('#bubble3').delay(1000).fadeIn('slow');
+    });
+    $('object').mouseleave(function(){
+        $('.signature').find('#bubble1, #bubble2, #bubble3').remove();
+    });
+});
